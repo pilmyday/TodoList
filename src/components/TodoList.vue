25de77578d21f0c6de9dcf570171c18552431e6f
Roy Yang <roy00374@gmail.com>
2023-06-17 00:11:30 +0800

    新增群組功能

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,9 +3,21 @@ import { ref, computed } from "vue";
 
 let id = 0;
 const hideCompleted = ref(false);
-const title = ref("TodoList");
+const title = ref("進階版TodoList");
 const newTodo = ref("");
-const todos = ref([{ id: id++, text: "Hello", done: true }]);
+const todos = ref([
+  {
+    id: id++,
+    isGroup: true,
+    name: "First",
+    data: [
+      {
+        text: "Hello",
+        done: true,
+      },
+    ],
+  },
+]);
 
 const filteredTodos = computed(() => {
   return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value;
@@ -19,19 +31,38 @@ function addTodo() {
 function removeTodo(todo) {
   todos.value = todos.value.filter((t) => t !== todo);
 }
+
+function addGroup() {
+  todos.value.push({
+    id: id++,
+    isGroup: true,
+    name: "new Group" + id,
+    data: [
+      {
+        text: newTodo.value,
+        done: false,
+      },
+    ],
+  });
+}
 </script>
 
 <template>
   <h1>{{ title }}</h1>
   <!-- submit.prevent能夠提交且不刷新頁面 -->
   <form @submit.prevent="addTodo">
-    <input v-model="newTodo" />
+    <!-- <input v-model="newTodo" /> -->
     <button>Add Todo</button>
+  </form>
+  <form @submit.prevent="addGroup">
+    <button>Add Group</button>
   </form>
   <ul>
     <li v-for="todo in filteredTodos" :key="todo.id">
       <input type="checkbox" v-model="todo.done" />
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+
+      <input v-model="todo.data[0].text" :class="{ done: todo.done }" />
+
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
